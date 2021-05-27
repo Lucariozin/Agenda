ff503c4a9e83e9858889c38ed342bf231f3696a6
Lucas Felix <73127948+Lucariozin@users.noreply.github.com>
2021-05-27 18:49:44 -0300

    estrutura do projeto

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,11 @@
+
+// Middlewares de segurança
+exports.checkCsrfError = (err, req, res, next) => {
+   if (err) return res.render('404');
+   next();
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+   res.locals.csrfToken = req.csrfToken();
+   next();
+};
